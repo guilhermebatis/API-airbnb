2038f0700d912b7159c24bc73e04dfaa4140b336
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-04-27 04:54:36 -0300

    feat: adiciona novos campos no modelo de Prediction

--- config/core/models.py
+++ config/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Prediction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    host_is_superhost = models.BooleanField()
+    host_total_listings_count = models.IntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    accommodates = models.IntegerField()
+    bathrooms = models.IntegerField()
+    bedrooms = models.IntegerField()
+    beds = models.IntegerField()
+    extra_people = models.FloatField()
+    minimum_nights = models.IntegerField()
+    number_of_reviews = models.IntegerField()
+    instant_bookable = models.BooleanField()
+    num_amenities = models.IntegerField()
+    property_type = models.CharField(max_length=100,)
+    cancellation_policy = models.CharField(
+        max_length=100,)
+
+    preco_previsto = models.FloatField()
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Predição - {self.preco_previsto}"
