799b3425bcfa75f0a9cf15e52cf47b3990c102ef
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-05-01 15:44:14 -0300

    test: implement access control tests (user restriction, admin access) and extend service test cases

--- config/core/tests/test_view.py
+++ config/core/tests/test_view.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from unittest.mock import patch
+from core.services.prediction_service import generate_prediction
 
 
 class PredictionTestCase(APITestCase):
@@ -75,3 +76,76 @@ class PredictionTestCase(APITestCase):
 
         self.assertEqual(response.data['predicted_price'], 500)
         mock_generate.assert_called_once()
+
+    def test_user_cannot_access_other_prediction(self):
+
+        data = {"host_is_superhost": True,
+                "host_total_listings_count": 5,
+                "latitude": -23.5,
+                "longitude": -46.6,
+                "accommodates": 3,
+                "bathrooms": 1,
+                "bedrooms": 2,
+                "beds": 2,
+                "extra_people": 50,
+                "minimum_nights": 2,
+                "number_of_reviews": 10,
+                "instant_bookable": True,
+                "num_amenities": 15,
+                "property_type": "apartment",
+                "cancellation_policy": "flexible"}
+
+        user_1 = User.objects.create_user(
+            username='user', password='pass123')
+        user_2 = User.objects.create_user(
+            username='otheruser', password='otherpass123')
+
+        prediction = generate_prediction(data, user_1)
+        prediction_id = prediction['id']
+
+        refresh = RefreshToken.for_user(user_2)
+        token = str(refresh.access_token)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        response = self.client.get(f'/api/predictions/{prediction_id}/')
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_admin_can_access_all_predictions(self):
+
+        user_1 = User.objects.create_user(
+            username='user', password='pass123')
+
+        admin_user = User.objects.create_user(
+            username='admin', password='adminpass123', is_staff=True)
+
+        data = {"host_is_superhost": True,
+                "host_total_listings_count": 5,
+                "latitude": -23.5,
+                "longitude": -46.6,
+                "accommodates": 3,
+                "bathrooms": 1,
+                "bedrooms": 2,
+                "beds": 2,
+                "extra_people": 50,
+                "minimum_nights": 2,
+                "number_of_reviews": 10,
+                "instant_bookable": True,
+                "num_amenities": 15,
+                "property_type": "apartment",
+                "cancellation_policy": "flexible"}
+
+        prediction = generate_prediction(data, user_1)
+        prediction_id = prediction['id']
+
+        refresh = RefreshToken.for_user(admin_user)
+        token = str(refresh.access_token)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        response = self.client.get(f'/api/predictions/{prediction_id}/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['id'], prediction_id)
+        self.assertIn('predicted_price', response.data)
