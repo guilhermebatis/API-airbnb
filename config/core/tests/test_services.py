ba4b149a238855df4574b001db10e507901bb648
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-05-01 15:13:19 -0300

    test: add validation for invalid input and multiple predictions

--- config/core/tests/test_services.py
+++ config/core/tests/test_services.py
@@ -34,3 +34,31 @@ class ServicesTestCase(TestCase):
         self.assertIsInstance(result['predicted_price'], (int, float))
         self.assertEqual(Prediction.objects.count(), 1)
         self.assertGreater(result['predicted_price'], 0)
+
+    def test_generate_prediction_invalid_input(self):
+        data = {}
+
+        with self.assertRaises(ValueError):
+            generate_prediction(data, self.user)
+
+    def test_generate_prediction_creates_multiple_records(self):
+        data = {"host_is_superhost": True,
+                "host_total_listings_count": 5,
+                "latitude": -23.5,
+                "longitude": -46.6,
+                "accommodates": 3,
+                "bathrooms": 1,
+                "bedrooms": 2,
+                "beds": 2,
+                "extra_people": 50,
+                "minimum_nights": 2,
+                "number_of_reviews": 10,
+                "instant_bookable": True,
+                "num_amenities": 15,
+                "property_type": "apartment",
+                "cancellation_policy": "flexible"}
+
+        generate_prediction(data, self.user)
+        generate_prediction(data, self.user)
+
+        self.assertEqual(Prediction.objects.count(), 2)
